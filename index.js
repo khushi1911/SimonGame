387857b792f879da48d01482ed20170d91cff775
khushi1911 <khushikaushik2030@gmail.com>
2026-07-04 18:01:38 +0530

    Update index.js
    sound

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var buttonColours = ["red", "blue", "green", "yellow"]; 
 var gamePattern =[];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
 
 function nextSequence(){
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
 
     var randomNumber = Math.floor(Math.random *4);
     var randomChosenColour = buttonColours[randomNumber];
@@ -12,7 +19,7 @@ function nextSequence(){
 }
 
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
 
